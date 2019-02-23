6d1417caaf90e04c4829b589b664584b4fa8b4b2
Jess Pearson <jessica.pearson@sky.uk>
2019-02-23 12:22:52 +0000

    Initial Commit

---
+++ Ultimate Flag Quiz/Controller/LaunchScreenViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ultimate Flag Quiz
+//
+//  Created by Pearson, Jessica (Tester) on 22/02/2019.
+//  Copyright © 2019 Pearson, Jessica. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
